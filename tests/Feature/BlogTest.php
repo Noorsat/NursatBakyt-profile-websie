e01fbc201baf79716b4394b3c104fed62d3bb859
Noorsat <itsnursat@gmail.com>
2021-03-15 16:49:51 +0600

    lab7

---
+++ tests/Feature/BlogTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class BlogTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_example()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    
+    public function test_blog_id_get_request(){
+        {
+            $response = $this->get('/blog/1');
+    
+            $response->assertStatus(200);
+        }
+    }
+    public function test_get_unexisted_client(){
+        {
+            $response = $this->get('/blog/10000');
+    
+            $response->assertStatus(404);
+        }
+    }
+    public function test_blog_responce(){
+        {
+            $response = $this->get('/blog/1');
+    
+            $response->assertViewHas('blog');
+        }
+    }
+}
